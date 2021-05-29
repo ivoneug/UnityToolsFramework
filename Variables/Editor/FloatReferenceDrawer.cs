e01ae1de6e9160f190ba7686b0e26f63092991e2
Evgeniy Ivon <ivoneug@gmail.com>
2021-05-29 21:54:30 +0300

    added existing framework files

---
+++ Variables/Editor/FloatReferenceDrawer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Framework.Variables
+{
+    [CustomPropertyDrawer(typeof(FloatReference))]
+    public class FloatReferenceDrawer : ReferenceDrawer
+    { }
+}
